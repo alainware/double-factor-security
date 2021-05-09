438badbde251ea9f475975fbbcb90d16207198f1
alainware <irving6258@gmail.com>
2021-05-09 00:23:53 -0500

    Commit: "Not a serious commit"

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
-import java.security.MessageDigest;
-import java.util.Arrays;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import javax.swing.JOptionPane;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Main
  */
 
 public class Main extends AES{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String msg = "";
         String msgToEncrypt = "";
         String KEY = "";
@@ -21,5 +19,21 @@ public class Main extends AES{
         msg = main.encryptAes(msgToEncrypt, KEY);
         JOptionPane.showMessageDialog(null, msg);
         JOptionPane.showMessageDialog(null, main.decryptAes(msg, KEY));
+        openSpam();
+    }
+    public void menu() {
+        System.out.println("Welcome to GunmetalCypher v0.1");
+        System.out.println();
+    }
+    public static void openSpam() throws IOException {
+        Desktop desktop = java.awt.Desktop.getDesktop();
+		try {
+			//specify the protocol along with the URL
+			URI oURL = new URI(
+					"https://www.caliente.mx");
+			desktop.browse(oURL);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
     }
 }
