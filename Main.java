3e44016e7e5a91c5988b66df04f72285516653e6
alainware <irving6258@gmail.com>
2021-05-12 10:57:07 -0500

    Commit: "Why so serious?"

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 import javax.swing.JOptionPane;
-import java.awt.Desktop;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * Main
  */
 
 public class Main extends AES{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String msg = "";
         String msgToEncrypt = "";
         String KEY = "";
@@ -19,21 +15,9 @@ public class Main extends AES{
         msg = main.encryptAes(msgToEncrypt, KEY);
         JOptionPane.showMessageDialog(null, msg);
         JOptionPane.showMessageDialog(null, main.decryptAes(msg, KEY));
-        openSpam();
     }
     public void menu() {
         System.out.println("Welcome to GunmetalCypher v0.1");
         System.out.println();
-    }
-    public static void openSpam() throws IOException {
-        Desktop desktop = java.awt.Desktop.getDesktop();
-		try {
-			//specify the protocol along with the URL
-			URI oURL = new URI(
-					"https://www.caliente.mx");
-			desktop.browse(oURL);
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
     }
 }
