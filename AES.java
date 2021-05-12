9aaba263940c9cc6f2dadb365f55dc8cfdbff39e
alainware <irving6258@gmail.com>
2021-05-12 15:36:13 -0500

    Commit: "AES working"

--- AES.java
+++ AES.java
@@ -1,53 +1,42 @@
-import java.security.MessageDigest;
-import java.util.Arrays;
-import java.util.Base64;
+import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 public class AES {
-    //Clave de encriptación/desencriptación
-    public SecretKeySpec createKey(String KEY) {
-        try {
-            // Transformar la llave en un arreglo de bytes
-            byte [] strKey = KEY.getBytes("UTF-8");
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            md.digest(strKey);
-            strKey = Arrays.copyOf(strKey, 16);
-            System.out.println(strKey);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(strKey, "AES");
-            return secretKeySpec;
-        } catch (Exception e) {
-            return null;
-        }
+    // Algorithm 
+    private static final String ALGORITHM = "AES";
+    // User defined key byte array
+    private byte [] uKey;
+    // Constructor
+    public AES (String key) {
+        uKey = key.getBytes();
     }
-    public String encryptAes(String messageToEncrypt, String KEY) {
-        try {
-            SecretKeySpec secretKeySpec = createKey(KEY);
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            byte [] strMessage = messageToEncrypt.getBytes("UTF-8");
-            byte [] encrypted = cipher.doFinal(strMessage);
-            Base64.Encoder encode = Base64.getEncoder();
-            String encryptedStrMessage = encode.encodeToString(encrypted);
-            System.out.println(encryptedStrMessage);
-            return encryptedStrMessage;
-        } catch (Exception e) {
-            return "";
-        }
+    // Encrypt AES
+    public String encryptAES (String messageData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte [] encVal = c.doFinal(messageData.getBytes("UTF-8"));
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encryptedValue = encoder.encodeToString(encVal);
+        return encryptedValue;
     }
-    public String decryptAes(String messageToDecrypt, String KEY) {
-        try {
-            SecretKeySpec secretKeySpec = createKey(KEY);
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-            Base64.Decoder decoder = Base64.getDecoder();
-            byte[] encodedDecryptedMessage = decoder.decode(messageToDecrypt);
-            byte [] decrypted = cipher.doFinal(encodedDecryptedMessage);
-            String decryptedStrMessage = new String(decrypted);
-            System.out.println(decryptedStrMessage);
-            return decryptedStrMessage;
-        } catch (Exception e) {
-            return "";
-        }
+    // Decrypt AES
+    public String decryptAES (String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.DECRYPT_MODE, key);
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte [] decodedValue = decoder.decode(encryptedData);
+        byte [] decoderMessage = c.doFinal(decodedValue);
+        String decryptedStrMessage = new String(decoderMessage);
+        System.out.println(decryptedStrMessage);
+        return decryptedStrMessage;
+    }
+    // Generate key for AES
+    private Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(uKey, ALGORITHM);
+        return key;
     }
 }
--- Main.java
+++ Main.java
@@ -1,23 +1,77 @@
-import javax.swing.JOptionPane;
-
 /**
  * Main
  */
-
+import java.util.Scanner;
 public class Main extends AES{
-    public static void main(String[] args) {
-        String msg = "";
-        String msgToEncrypt = "";
-        String KEY = "";
-        Main main = new Main();
-        msgToEncrypt = JOptionPane.showInputDialog("Ingresa la cadena a encriptar con AES 128");
-        KEY = JOptionPane.showInputDialog("Ingresa la llave a encriptar con AES 128");
-        msg = main.encryptAes(msgToEncrypt, KEY);
-        JOptionPane.showMessageDialog(null, msg);
-        JOptionPane.showMessageDialog(null, main.decryptAes(msg, KEY));
+    public Main(String key) {
+        super(key);
     }
-    public void menu() {
+    private static Scanner sc = new Scanner(System.in);
+    private static String uKey = "";
+    private static String msg = "";
+    private static String encData = "";
+    private static String decData = "";
+    public static void main(String[] args) {
+        int option = 0;
+        try {
+            do {
+                menu();
+                option = sc.nextInt();
+                switch (option) {
+                    case 1:
+                    System.out.println("Please enter a 16 character key: ");
+                    uKey = sc.nextLine();
+                    while (checkKey(uKey) != true) {
+                        uKey = sc.nextLine();
+                    }
+                    AES aesEnc = new AES(uKey);
+                    System.out.println("Please write a message to hide: ");
+                    msg = sc.nextLine();
+                    encData = aesEnc.encryptAES(msg);
+                    System.out.println("Message was encrypted succesfully!");
+                    System.out.println("The message is: " + encData); 
+                        break;
+                    case 2:
+                    System.out.println("Please enter a 16 character key: ");
+                    uKey = sc.nextLine();
+                    while (checkKey(uKey) != true) {
+                        uKey = sc.nextLine();
+                    }
+                    AES aesDec = new AES(uKey);
+                    System.out.println("Please write the secret message: ");
+                    msg = sc.nextLine();
+                    decData = aesDec.decryptAES(msg);
+                    System.out.println("Message was decrypted succesfully!"); 
+                    System.out.println("...");            
+                    System.out.println("The message is: " + decData);       
+                        break;                
+                    default:
+                        break;
+                }
+            } while (option != 3);
+
+        } catch (Exception e) {
+            System.out.println("System error!");
+        }
+        sc.close();
+    }
+    public static void menu() {
+        System.out.println("==============================");
         System.out.println("Welcome to GunmetalCypher v0.1");
-        System.out.println();
+        System.out.println("==============================");
+        System.out.println("\n");
+        System.out.println("1. Encrypt message");
+        System.out.println("2. Decrypt message");
+        System.out.println("3. Exit");
+        System.out.print("Option: ");
+    }
+    public static boolean checkKey(String uKey) {
+        if (uKey.length() == 16) {
+            System.out.println("Key - OK!");
+            return true;
+        } else {
+            System.out.println("Key ERROR!\nThe key length must be of 16 characters!");
+            return false;
+        }
     }
 }
