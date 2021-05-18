990d1a143926e5cec8c5cc4cdd720a8a0423c5f3
irvingpercam <a01703171@itesm.mx>
2021-05-18 17:05:35 -0500

    Update: "MultiAES functions ready!"

--- MultiAES.java
+++ MultiAES.java
@@ -9,15 +9,6 @@ public class MultiAES extends AES{
     private static String uKey = "";
     private static String msg = "";
     public static void main(String[] args) {
-        // Add to array usage
-        // String[] words = {"Hola", "mundo"};
-        // words = addToArray(words, "perros");
-        // System.out.println(Arrays.toString(words));
-
-        // Split message usage
-        // String message = "El cielo resplandece a mi alrededor";
-        // String[] splittedMessage = message.split(" ");
-        // System.out.println(Arrays.toString(splittedMessage));
         int option = 0;
         try {
             do {
@@ -30,44 +21,13 @@ public class MultiAES extends AES{
                     while (checkKey(uKey) != true) {
                         uKey = sc.nextLine();
                     }
-                    AES aesEnc = new AES(uKey);
                     System.out.println("Please write a message to hide: ");
                     msg = sc.nextLine();
                     while(msg.length() == 0){
                         System.out.println("Please write a valid message: ");
                         msg = sc.nextLine();
                     }
-                    /*
-                    [START] Encrypting loop
-                    */
-                    //encData = aesEnc.encryptAES(msg);
-                    
-                    // Step 1. Convert the message to a String array
-
-                    String[] messageArray = msg.split(" ");
-
-                    // Step 2. Create a new String Array for storing the encrypted words
-
-                    String[] encryptedMessageArray = {};
-
-                    // Step 3. Encrypt each word inside messageArray
-
-                    for (String s : messageArray) {
-                        encryptedMessageArray = addToArray(encryptedMessageArray, aesEnc.encryptAES(s));
-                    }
-
-                    // Step 4. Append the ecnrypted strings to a StringBuilder
-
-                    StringBuilder finalMessage = new StringBuilder("");
-                    for (String string : encryptedMessageArray) {
-                        finalMessage.append(string);
-                    }
-                    System.out.println(finalMessage);
-                    /*
-                    [END] Encrypting loop
-                    */
-                    System.out.println("Message was encrypted succesfully!");
-                    // System.out.println("The message is: " + encData); 
+                    System.out.println("\n\nTop secret message: " + encrypt(msg, uKey));
                         break;
                     case 2:
                     System.out.println("Please enter a 16 character key: ");
@@ -75,52 +35,13 @@ public class MultiAES extends AES{
                     while (checkKey(uKey) != true) {
                         uKey = sc.nextLine();
                     }
-                    AES aesDec = new AES(uKey);
                     System.out.println("Please write the secret message: ");
                     msg = sc.nextLine();
                     while(msg.length() == 0){
                         System.out.println("Please write a valid message: ");
                         msg = sc.nextLine();
                     }
-                    /*
-                    [START] Decrypting loop
-                    */
-                    //decData = aesDec.decryptAES(msg);
-                    
-                    // Step 1. Convert the message to a String Array
-
-                    String[] encMessageArray = msg.split("==");
-
-                    // Step 2. Create a new String Array for storing the decrypted words
-
-                    String[] decryptedMessageArray = {};
-
-                    // Step 3. Encrypt each word inside messageArray
-
-                    for (String string : encMessageArray) {
-                        decryptedMessageArray = addToArray(decryptedMessageArray, aesDec.decryptAES(string));
-                    }
-
-                    // Step 4. Append the decrypted strings to a StringBuilder
-
-                    StringBuilder finalDecryptedMessage = new StringBuilder("");
-                    // for (String string : encryptedMessageArray) {
-                    //     finalDecryptedMessage.append(string + " ");
-                    // }
-                    for (int i = 0; i < decryptedMessageArray.length; i++) {
-                        if (i == decryptedMessageArray.length - 1) {
-                            finalDecryptedMessage.append(decryptedMessageArray[i] + ".");
-                        } else {
-                            finalDecryptedMessage.append(decryptedMessageArray[i] + " ");
-                        }
-                    }
-                    System.out.println(finalDecryptedMessage);
-                    /*
-                    [END] Decrypting loop
-                    */
-                    System.out.println("Message was decrypted succesfully!"); 
-                    // System.out.println("...");            
-                    // System.out.println("The message is: " + decData);       
+                    System.out.println("\n\nTop secret message: " + decrypt(msg, uKey));
                         break;                
                     default:
                         break;
@@ -139,6 +60,7 @@ public class MultiAES extends AES{
         arr[arr.length - 1] = string; // Assign 40 to the last element
         return arr;
     }
+    
     public static boolean checkKey(String uKey) {
         if (uKey.length() == 16) {
             System.out.println("Key - OK!");
@@ -147,6 +69,88 @@ public class MultiAES extends AES{
             System.out.println("Key ERROR!\nThe key length must be of 16 characters!");
             return false;
         }
+    }
+
+    public static String encrypt(String msg, String uKey) {
+        /*
+        [START] Encrypting loop
+        */
+        // Create AES Object
+        AES aesEnc = new AES(uKey);
+
+        // Step 1. Convert the message to a String array
+
+        String[] messageArray = msg.split(" ");
+
+        // Step 2. Create a new String Array for storing the encrypted words
+
+        String[] encryptedMessageArray = {};
+
+        // Step 3. Encrypt each word inside messageArray
+
+        for (String s : messageArray) {
+            try {
+                encryptedMessageArray = addToArray(encryptedMessageArray, aesEnc.encryptAES(s));
+            } catch (Exception e) {
+                System.out.println("Encryption failed for MultiAES class!");
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Message was encrypted succesfully!");
+
+        // Step 4. Append the ecnrypted strings to a StringBuilder
+
+        StringBuilder finalMessage = new StringBuilder("");
+        for (String string : encryptedMessageArray) {
+            finalMessage.append(string);
+        }
+        return finalMessage.toString();
+        /*
+        [END] Encrypting loop
+        */
+    }
+    public static String decrypt(String msg, String uKey) {
+        /*
+        [START] Decrypting loop
+        */
+        // Create AES Object
+        AES aesDec = new AES(uKey);
+        
+        // Step 1. Convert the message to a String Array
+
+        String[] encMessageArray = msg.split("==");
+
+        // Step 2. Create a new String Array for storing the decrypted words
+
+        String[] decryptedMessageArray = {};
+
+        // Step 3. Encrypt each word inside messageArray
+
+        for (String string : encMessageArray) {
+            try {
+                decryptedMessageArray = addToArray(decryptedMessageArray, aesDec.decryptAES(string));
+            } catch (Exception e) {
+                System.out.println("Decryption failed for MultiAES class!");
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Message was decrypted succesfully!");
+
+        // Step 4. Append the decrypted strings to a StringBuilder
+
+        StringBuilder finalDecryptedMessage = new StringBuilder("");
+
+        for (int i = 0; i < decryptedMessageArray.length; i++) {
+            if (i == decryptedMessageArray.length - 1) {
+                finalDecryptedMessage.append(decryptedMessageArray[i] + ".");
+            } else {
+                finalDecryptedMessage.append(decryptedMessageArray[i] + " ");
+            }
+        }
+        return finalDecryptedMessage.toString();
+        /*
+        [END] Decrypting loop
+        */
     }
     public static void menu() {
         System.out.println("==============================");
